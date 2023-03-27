7364d3c801f7bc053b30d9f3d4ee65fe34f6f5f9
vick <victordickson474@gmail.com>
2023-03-27 15:21:57 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# cw-todo-app
+![image](https://user-images.githubusercontent.com/68965446/215503794-3b430a2c-bdd1-4527-9dbc-3e2f4b03e3ac.png)
+
+This project aims to create a Jenkins pipeline to deploy web-page written Nodejs and React Frameworks on AWS Cloud Infrastructure using Ansible.
+
+Building infrastructure process is managed with control node utilizing Ansible. This infrastructure has 1 Jenkins Server (`Amazon Linux 2 AMI`) as Ansible Control Node and 3 EC2's as worker node (`Red Hat Enterprise Linux 8 with High Availability`).
+
+These EC2's will be launched on AWS console. Web-page has 3 main components which are Postgresql, Nodejs, and React. Each component is serving in Docker container on EC2s dedicated for them. 
+
+Postgresql is serving as Database of web-page. Nodejs controls backend part of web-side and react controls frontend side of web-page. The Web Application will be deployed using Nodejs and React framework and should be accessible via web browser from anywhere on port 3000.
+
+In my case; first of all I launched Jenkins Server with security group allowing SSH (port 22) and HTTP (ports 80, 8080) connections. After that, I have followed the steps indicate below:
+
+- Create infrastructure with Terraform,
+- Create an image repository on ECR for the app,
+- Build the application Docker image and push it to the same ECR repository with different tags,
+- Deploy the application on AWS EC2's with Ansible,
+- Make a failure stage and ensure to destroy infrastructure, ECR repo and docker images when the pipeline failed.
+
+I used Jenkins Pipeline for building, testing, deploying and facilitating continuous integration and continuous delivery.
+Terraform and Ansible for infrastructure provisioning, configuration management and automation. 
+
+![jenkins-pipeline](https://user-images.githubusercontent.com/68965446/227952951-e25dfc3a-e498-470e-87b9-76f6b2503971.PNG)
+
+
+![mysite](https://user-images.githubusercontent.com/68965446/227950521-ecbe0810-cc6a-4afd-8217-2121b9e06843.PNG)
+
---
+++ main.tf
@@ -0,0 +1,138 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  backend "s3" {
+    bucket = "cw-todo-app"
+    key = "backend/tf-backend-jenkins.tfstate"
+    region = "us-east-1"
+    
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "tags" {
+  default = ["postgresql", "nodejs", "react"]
+}
+
+resource "aws_iam_role" "ec2" {
+  name = "ec2-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "ecr_access_policy" {
+  name        = "ecr_access_policy"
+  description = "ECR Access Policy"
+  policy      = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:DescribeImages",
+          "ecr:GetAuthorizationToken"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_access_attachment" {
+  policy_arn = aws_iam_policy.ecr_access_policy.arn
+  role       = aws_iam_role.ec2.name
+}
+
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "ec2_instance_profile"
+  
+  role = aws_iam_role.ec2.name
+}
+
+resource "aws_instance" "managed_nodes" {
+  ami = "ami-0f095f89ae15be883"
+  count = 3
+  instance_type = "t2.micro"
+  key_name = "keypair-name"
+  vpc_security_group_ids = [aws_security_group.tf-sec-gr.id]
+  iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
+  tags = {
+    Name = "ansible_${element(var.tags, count.index )}"
+    stack = "ansible_project"
+    environment = "development_1"
+  }
+}
+
+resource "aws_security_group" "tf-sec-gr" {
+  name = "project208-sec-gr"
+  tags = {
+    Name = "project208-sec-gr"
+  }
+
+  ingress {
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 5000
+    protocol    = "tcp"
+    to_port     = 5000
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 3000
+    protocol    = "tcp"
+    to_port     = 3000
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 5432
+    protocol    = "tcp"
+    to_port     = 5432
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    protocol    = -1
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "react_ip" {
+  value = "http://${aws_instance.managed_nodes[2].public_ip}:3000"
+}
+
+output "node_public_ip" {
+  value = aws_instance.managed_nodes[1].public_ip
+
+}
+
+output "postgre_private_ip" {
+  value = aws_instance.managed_nodes[0].private_ip
+
+}
